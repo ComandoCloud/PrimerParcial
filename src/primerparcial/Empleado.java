2102b028c8a5f9c5b61f9a34c86afb19751f6010
ALeeh <aleehmoyya@gmail.com>
2021-05-17 16:45:04 -0300

    Subo Clase Empleados

---
+++ src/primerparcial/Empleado.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerparcial;
+
+/**
+ *
+ * @author ALeeh
+ */
+public class Empleado {
+    public String Nombre;
+    public String Apellido;
+    public int numSeguroSocial;
+    
+    public Empleado(){
+        
+    }
+    
+    public Empleado(String Nombre,String Apellido,int numero){
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.numSeguroSocial = numero;
+    }
+    
+    @Override
+    public String toString(){
+        return this.Nombre.toString() + " , "+ this.Apellido.toString();
+    }
+    
+    public double Ingresos(double MontoTotal){
+        return MontoTotal;    
+    }
+}
