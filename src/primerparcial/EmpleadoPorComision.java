3afe72823b1bfeabced95495f4e8289db00c590f
WamarpeMaximilianoKiss <mkiss@wamarpe.com.ar>
2021-05-17 16:37:24 -0300

    Clases uml

---
+++ src/primerparcial/EmpleadoPorComision.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerparcial;
+
+/**
+ *
+ * @author Maximiliano Kiss
+ */
+public class EmpleadoPorComision {
+    private double salarioBase;
+    private int cantidadReparaciones;
+    private double tarifaComision;
+
+    /**
+     * @return the salarioBase
+     */
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    /**
+     * @param salarioBase the salarioBase to set
+     */
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    /**
+     * @return the cantidadReparaciones
+     */
+    public int getCantidadReparaciones() {
+        return cantidadReparaciones;
+    }
+
+    /**
+     * @param cantidadReparaciones the cantidadReparaciones to set
+     */
+    public void setCantidadReparaciones(int cantidadReparaciones) {
+        this.cantidadReparaciones = cantidadReparaciones;
+    }
+
+    /**
+     * @return the tarifaComision
+     */
+    public double getTarifaComision() {
+        return tarifaComision;
+    }
+
+    /**
+     * @param tarifaComision the tarifaComision to set
+     */
+    public void setTarifaComision(double tarifaComision) {
+        this.tarifaComision = tarifaComision;
+    }
+    
+}
---
+++ src/primerparcial/EmpleadoPorHoras.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerparcial;
+
+/**
+ *
+ * @author Maximiliano Kiss
+ */
+public class EmpleadoPorHoras {
+    
+    private double sueldoPorHora;
+    private int horasTrabajadas;
+
+    /**
+     * @return the sueldoPorHora
+     */
+    public double getSueldoPorHora() {
+        return sueldoPorHora;
+    }
+
+    /**
+     * @param sueldoPorHora the sueldoPorHora to set
+     */
+    public void setSueldoPorHora(double sueldoPorHora) {
+        this.sueldoPorHora = sueldoPorHora;
+    }
+
+    /**
+     * @return the horasTrabajadas
+     */
+    public int getHorasTrabajadas() {
+        return horasTrabajadas;
+    }
+
+    /**
+     * @param horasTrabajadas the horasTrabajadas to set
+     */
+    public void setHorasTrabajadas(int horasTrabajadas) {
+        this.horasTrabajadas = horasTrabajadas;
+    }
+}
